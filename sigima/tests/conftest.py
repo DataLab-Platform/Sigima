3f0e0930ff2316301484f27a101afd6c3b54322c
Pierre Raybaut <p.raybaut@codra.fr>
2025-07-08 17:26:22 +0200

    Fix environment variable name in pytest report header

--- sigima/tests/conftest.py
+++ sigima/tests/conftest.py
@@ -57,7 +57,7 @@ def pytest_report_header(config):  # pylint: disable=unused-argument
     except ImportError:
         pass
     envlist = []
-    for vname in ("CDL_DATA", "PYTHONPATH", "DEBUG", "QT_API", "QT_QPA_PLATFORM"):
+    for vname in ("SIGIMA_DATA", "PYTHONPATH", "DEBUG", "QT_API", "QT_QPA_PLATFORM"):
         value = os.environ.get(vname, "")
         if value:
             if vname == "PYTHONPATH":
